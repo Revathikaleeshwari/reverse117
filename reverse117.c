abcebd0d5c6843a2230d30e014e37b0042c37591
Revathi N <nrevathibe38@gmail.com>
2016-07-05 15:52:03 +0530

    Create reverse117.c

---
+++ reverse117.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+int i;
+char s[50];
+gets(s);
+for(i=1-i;i>0;i--)
+{
+printf("enter a string:");
+scanf("%c", s[i]);
+printf("-");
+}
+return 0;
+}
